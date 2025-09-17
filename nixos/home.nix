aa758f851f46c24761538fb60cb56694eab795cf
LFour <63118374+LFour86@users.noreply.github.com>
2025-09-17 18:30:13 +0800

    Update home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,22 +3,31 @@
   #Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   home.username = "lfour";
+  xdg.configFile."powerdevilrc".text = ''
+      [AC][DPMSControl]
+      idleTime=7200000 # ms, 0 --> disable
+      sleepTime=0 # s, 0 --> disable
+
+      [Battery][DPMSControl]
+      idleTime=1200000 # ms
+      sleepTime=1800   # s
+  '';
   home.homeDirectory = "/home/lfour";
   imports = [
 	./home/llm.nix
 	./home/programs.nix 
 	./home/userpkgs.nix
 	./home/config/fastfetch.nix
-	#./home/config/hyprland/dunst.nix
-	#./home/config/hyprland/hypridle.nix
-	#./home/config/hyprland/hyprland.nix
-	#./home/config/hyprland/hyprlock.nix
-	#./home/config/hyprland/waybar-config.nix
-	#./home/config/hyprland/waybar-style-css.nix
-	#./home/config/hyprland/wofi.nix 
-	#./home/config/kitty.nix
+	./home/config/hyprland/dunst.nix
+	./home/config/hyprland/hypridle.nix
+	./home/config/hyprland/hyprland.nix
+	./home/config/hyprland/hyprlock.nix
+	./home/config/hyprland/waybar-config.nix
+	./home/config/hyprland/waybar-style-css.nix
+	./home/config/hyprland/wofi.nix 
   ];
 
   #Home-manager version
   home.stateVersion = "25.11";
 }
+
