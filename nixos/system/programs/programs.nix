b00434cb0c0a0af6046d1f4f256c7d2781059fe2
LFour <63118374+LFour86@users.noreply.github.com>
2025-09-20 19:08:09 +0800

    Update programs.nix

--- nixos/system/programs/programs.nix
+++ nixos/system/programs/programs.nix
@@ -22,16 +22,6 @@
     vimAlias = true;
   };
 
-  # Emacs-Unstable 
-  #services.emacs.enable = true;
-  #services.emacs.package = pkgs.emacs-unstable;
-  #nixpkgs.overlays = [
-  #  (import (builtins.fetchTarball {
-  #    url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-  #    sha256 = "0ad9qjz1y1n2hy69ixl1mbzd0mn19070765v5gdp3b19pggsii4w";
-  #  }))
-  #];
-
-  # Nix ld 
-  #programs.nix-ld.enable = true;
+  # Nix-ld: Run closed-source Binary
+  programs.nix-ld.enable = true;
 }
