9e4e16663ab3d0ebad1bb691bfd2b565a2771883
LFour <63118374+LFour86@users.noreply.github.com>
2025-09-20 17:06:21 +0800

    Update security.nix

--- nixos/system/config/security.nix
+++ nixos/system/config/security.nix
@@ -143,8 +143,8 @@
 
   # Enable USB Guard
   services.usbguard = {
-    enable = false;
-    dbus.enable = false;
+    enable = true;
+    dbus.enable = true;
     implicitPolicyTarget = "block";
     # TODO set yours pref USB devices (change {id} to your trusted USB device),
     #use "lsusb" command (from usbutils package) to get list of all connected USB devices
