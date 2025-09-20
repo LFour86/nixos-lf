81cfccf471603268180c2a5a986135d8ba054f78
LFour <63118374+LFour86@users.noreply.github.com>
2025-09-20 17:05:37 +0800

    Update network.nix

--- nixos/system/config/network.nix
+++ nixos/system/config/network.nix
@@ -4,6 +4,16 @@
   networking.hostName = "nixos"; # Define hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
+  #networking.networkmanager.dns = "dnsmasq";
+  #services.dnsmasq.enable = true;
+  #networking.firewall.trustedInterfaces = [ "wlo1" ];
+
+  # 可选：启用 NAT 让热点设备能上网
+  networking.nat = {
+      enable = true;
+      externalInterface = "enp4s0";   # 外网接口（例如有线网卡或 PPPoE）
+      internalInterfaces = [ "wlo1" ];  # 内部热点接口
+  };
 
   # Substituters mirrors
   nix = {
@@ -18,9 +28,8 @@
 
   #hosts
   networking.hosts = {
-    "20.27.177.113" = ["github.com"];
+    #"20.27.177.113" = ["github.com"];
   };
 
   environment.systemPackages = with pkgs; [];
 }
-
