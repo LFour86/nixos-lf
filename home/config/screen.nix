f69be22c1d40da95a6a5546f221e919dd999e337
LFour <63118374+LFour86@users.noreply.github.com>
2025-09-25 00:36:49 -0400

    Update screen.nix

--- home/config/screen.nix
+++ home/config/screen.nix
@@ -1,26 +1,25 @@
 { ... }:
 {
   # Gnome
-  dconf.settings = {
-    "org/gnome/desktop/session" = {
-      idle-delay = 1800;
-    };
+  #dconf.settings = {
+    #"org/gnome/desktop/session" = {
+      #idle-delay = 1800;
+      #};
 
-    "org/gnome/settings-daemon/plugins/power" = {
-      sleep-inactive-ac-type = "nothing";
-      sleep-inactive-battery-type = "nothing";
-    };
-  };
+    #"org/gnome/settings-daemon/plugins/power" = {
+      #sleep-inactive-ac-type = "nothing";
+      #sleep-inactive-battery-type = "nothing";
+      #};
+  #};
 
   # KDE Idle
-  #xdg.configFile."powerdevilrc".text = ''
-      #[AC][DPMSControl]
-      #idleTime=7200000 # ms, 0 --> disable
-      #sleepTime=0 # s, 0 --> disable
+  xdg.configFile."powerdevilrc".text = ''
+      [AC][DPMSControl]
+      idleTime=7200000 # ms, 0 --> disable
+      sleepTime=0 # s, 0 --> disable
 
-      #[Battery][DPMSControl]
-      #idleTime=1200000 # ms
-      #sleepTime=1800   # s
-  #'';
+      [Battery][DPMSControl]
+      idleTime=1200000 # ms
+      sleepTime=1800   # s
+  '';
 }
-
