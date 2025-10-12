e03489843cb4e74ef870982ac36efa01ce67534e
LFour <63118374+LFour86@users.noreply.github.com>
2025-10-12 17:49:47 +0800

    Update desktop.nix

--- system/config/desktop.nix
+++ system/config/desktop.nix
@@ -18,12 +18,12 @@
   };
 
   # Colorful
-  #services.colord.enable = true;
+  services.colord.enable = true;
   # Location
-  #services.geoclue2.enable = true;
-  #programs.kdeconnect.enable = true;
+  services.geoclue2.enable = true;
+  programs.kdeconnect.enable = true;
   # Disk
-  #programs.partition-manager.enable = true;
+  programs.partition-manager.enable = true;
   # Indusrial I/O
   hardware.sensor.iio.enable = true;
   # Qt theme
@@ -60,21 +60,21 @@
     #adwaita-qt6
     
     # KDE
-    #kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
-    #kdePackages.kcalc # Calculator
-    #kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
-    #kdePackages.kclock
-    #kdePackages.kcolorchooser # A small utility to select a color
-    #kdePackages.kolourpaint # Easy-to-use paint program
-    #kdePackages.krdc
-    #kdePackages.ksystemlog # KDE SystemLog Application
-    #kdePackages.sddm-kcm # Configuration module for SDDM
-    #kdiff3 # Compares and merges 2 or 3 files or directories
-    #kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
-    #kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
-    #hardinfo2 # System information and benchmarks for Linux systems
-    #haruna # Open source video player built with Qt/QML and libmpv
-    #wayland-utils # Wayland utilities
-    #wl-clipboard # Command-line copy/paste utilities for Wayland
+    kdePackages.discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
+    kdePackages.kcalc # Calculator
+    kdePackages.kcharselect # Tool to select and copy special characters from all installed fonts
+    kdePackages.kclock
+    kdePackages.kcolorchooser # A small utility to select a color
+    kdePackages.kolourpaint # Easy-to-use paint program
+    kdePackages.krdc
+    kdePackages.ksystemlog # KDE SystemLog Application
+    kdePackages.sddm-kcm # Configuration module for SDDM
+    kdiff3 # Compares and merges 2 or 3 files or directories
+    kdePackages.isoimagewriter # Optional: Program to write hybrid ISO files onto USB disks
+    kdePackages.partitionmanager # Optional Manage the disk devices, partitions and file systems on your computer
+    hardinfo2 # System information and benchmarks for Linux systems
+    haruna # Open source video player built with Qt/QML and libmpv
+    wayland-utils # Wayland utilities
+    wl-clipboard # Command-line copy/paste utilities for Wayland
   ];
 }
