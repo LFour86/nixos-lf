c46e0b0297c8caf7eafaf8776e9de308f0da84e1
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-03 12:50:20 +0800

    Update Bilibili version and file path

--- overlays/bilibili.nix
+++ overlays/bilibili.nix
@@ -1,13 +1,15 @@
 final: prev: let
-  version = "1.17.2-1";
+  version = "1.17.2-2";
   fname = "io.github.msojocs.bilibili_${version}_amd64.deb";
-  path = "/home/lfour/LocalApps/${fname}";
-  shaHash = "0jmid5miq5a4f306sxkxdgranvnb9h8s0902355jccb44k7r4w3i";
+  absPath = "/etc/nixos/overlays/LocalApps/${fname}";
 in {
   bilibili-local = prev.bilibili.overrideAttrs (old: {
+    version = version;
     src = prev.fetchurl {
-      url = "file://${path}";
-      sha256 = shaHash;
+      url = "file://${absPath}";
+      # nix-prefetch-url file:///etc/nixos/overlays/LocalApps/io.github.msojocs.bilibili_1.17.2-2_amd64.deb
+      # 👇
+      sha256 = "0xvhg731629jz24a92qg9ls67glwibfsa2ja1kdxbyj3f1nkan55";
     };
   });
 }
