f16e48caee1990f928f713c032f68659f7346e9a
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-03 12:50:48 +0800

    Update ollama service configuration
    Reorganize services configuration for ollama.

--- home/programs/llm.nix
+++ home/programs/llm.nix
@@ -1,10 +1,24 @@
 { pkgs, ... }:
 {
-  services.ollama.enable = false;
-  services.ollama.acceleration = "cuda";
+  #services.llama-cpp = {
+    #enable = false;
+    #extraFlags = [
+      #"-c"
+      #"4096"
+      #"-ngl"
+      #"32"
+      #"--numa"
+      #"numactl"
+    #];
+  #};
+
+  services.ollama = {
+    enable = false;
+    acceleration = "cuda";
+  };
+
   home.packages = with pkgs; [
     ollama
     llama-cpp
   ];
 }
-
