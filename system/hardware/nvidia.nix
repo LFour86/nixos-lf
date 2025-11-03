f2980e1a18aef7d2693200f03a1fc572f831be38
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-03 12:54:16 +0800

    Refactor NVIDIA configuration in nvidia.nix

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -1,41 +1,38 @@
 { config, lib, pkgs, modulesPath, ...}:
 {
-
   # Enable OpenGL
   hardware.graphics.enable = true;
   
-   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
     
-   hardware.nvidia.prime = {
-  	
-  	        #offload = {
-		    #enable = true;
-		    #enableOffloadCmd = true;
-		#};
-		# Make sure to use the correct Bus ID values for your system!
-		amdgpuBusId = "PCI:6:0:0";
-		nvidiaBusId = "PCI:1:0:0";
-	};
+  hardware.nvidia.prime = {
+    #offload = {
+      #enable = true;
+      #enableOffloadCmd = true;
+    #};
+    # Make sure to use the correct Bus ID values for your system!
+    amdgpuBusId = "PCI:6:0:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
 	
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
-  
   
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.powerManagement.enable = false;
   hardware.nvidia.powerManagement.finegrained = false;
   hardware.nvidia.open = false;
   hardware.nvidia.nvidiaSettings = true;
+ 
+  # Bootloader
+  boot.initrd.kernelModules = [ "nvidia" ];
   
-   #If you encounter the problem of booting to text mode you might try adding the Nvidia kernel module manually with this
-   #boot.initrd.kernelModules = [ "nvidia" ];
-   #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-    
+  # Fixed
+  #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
     
   #disable nouveau
   boot.kernelParams = [ "modprobe.blacklist=nouveau" "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
     
-   #Screen Tearing Issues
-   hardware.nvidia.forceFullCompositionPipeline = true;
+  #Screen Tearing Issues
+  hardware.nvidia.forceFullCompositionPipeline = true;
 }
-
