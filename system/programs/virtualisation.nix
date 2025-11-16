911d767982993d83292a9d07992b8c73eb4fa565
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-16 11:36:15 +0800

    Enable Containerd and Docker in virtualisation.nix

--- system/programs/virtualisation.nix
+++ system/programs/virtualisation.nix
@@ -2,7 +2,6 @@
 {
   # KVM
   programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = ["lfour"];
   virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.libvirtd = {
     enable = true;
@@ -14,50 +13,21 @@
     };
    };
    
-  #networking.interfaces.enp4s0.ipv4.addresses = [{
-  #address = "10.25.0.2";
-  #prefixLength = 24;
-  #}];
-  
   # Enable Containerd
-  # virtualisation.containerd.enable = true;
+  virtualisation.containerd.enable = true;
 
   # Enable Docker
-  # virtualisation.docker.enable = true;
-  # virtualisation.docker.rootless = {
-  #   enable = true;
-  #   setSocketVariable = true;
-  # };
-  # users.extraGroups.docker.members = [ "nixos" ];
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
 
-  # Enable Podman
-  #virtualisation = {
-   #podman = {
-   #   enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-     # dockerCompat = true;
-    #  dockerSocket.enable = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-   #   defaultNetwork.settings.dns_enabled = true;
-   # };
-  #};
-  #environment.variables.DBX_CONTAINER_MANAGER = "podman";
-  #users.extraGroups.podman.members = [ "nixos" ];
-
-  #environment.systemPackages = with pkgs; [
-    #nerdctl
-    #firecracker
-    #firectl
-    #flintlock
-    #distrobox
-    #qemu
-    #docui
-    #podman-compose
-    #podman-tui
-    #docker-compose
-    #lazydocker
-    #docker-credential-helpers
-  #];
+  environment.systemPackages = with pkgs; [
+    docker-compose
+    docui
+  ];
 }
+
