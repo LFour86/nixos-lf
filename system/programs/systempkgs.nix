d50d872291482bbefa726d5a8765ab9f60477a01
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-16 11:36:00 +0800

    Refactor Qt packages into a single list

--- system/programs/systempkgs.nix
+++ system/programs/systempkgs.nix
@@ -47,52 +47,6 @@
     llvmPackages_latest.libllvm
     llvmPackages_latest.libcxx
     llvmPackages_latest.clang
-    # Qt
-    kdePackages.qt3d
-    kdePackages.qtsvg
-    kdePackages.qtdoc
-    kdePackages.qt6ct
-    kdePackages.qtmqtt
-    kdePackages.qtgrpc
-    kdePackages.qtbase
-    kdePackages.qttools
-    kdePackages.qtspell
-    kdePackages.qtscxml
-    kdePackages.qt6gtk2
-    kdePackages.qtspeech
-    kdePackages.qtlottie
-    kdePackages.qtgraphs
-    kdePackages.qtcharts
-    kdePackages.qtwebview
-    kdePackages.qtwayland
-    kdePackages.qtsensors
-    kdePackages.qtquick3d
-    kdePackages.qt5compat
-    kdePackages.qtlocation
-    kdePackages.qtkeychain
-    kdePackages.qtwebengine
-    kdePackages.qtutilities
-    kdePackages.qtserialbus
-    kdePackages.qtdatavis3d
-    kdePackages.qtwebsockets
-    kdePackages.qtwebchannel
-    kdePackages.qtserialport
-    kdePackages.qtmultimedia
-    kdePackages.qthttpserver
-    kdePackages.qtshadertools
-    kdePackages.qtpositioning
-    kdePackages.qtnetworkauth
-    kdePackages.qtdeclarative
-    kdePackages.qttranslations
-    kdePackages.qtimageformats
-    kdePackages.qtconnectivity
-    kdePackages.qtremoteobjects
-    kdePackages.qtquicktimeline
-    kdePackages.qtquick3dphysics
-    kdePackages.qtpbfimageplugin
-    kdePackages.qtlanguageserver
-    kdePackages.qtvirtualkeyboard
-    kdePackages.qtquickeffectmaker
     #
     ###### JAVA ##################
     jre21_minimal
@@ -122,101 +76,33 @@
     esptool
     #
     ###### Python #################
-    python313
-    python313Packages.openai-whisper
-    python313Packages.torch
-    python313Packages.sounddevice
+    python311
     ###### NodeJS #################
-    nodejs
+    nodejs_24
     ###### Rust ###################
     cargo
     rustc
     ###### GO ####################
     go
-    ###### Others ################
-    # Matlab
-    (buildFHSEnvBubblewrap {
-      name = "matlab-fhs";
-      chdir = "/home/lfour";
-      targetPkgs = pkgs: with pkgs; [
-      	alsa-lib
-        atk
-        cairo
-      	cups
-      	dbus
-      	expat
-      	fontconfig
-      	freetype
-      	gdk-pixbuf
-      	glib
-      	gtk3
-	xorg.libX11
-	xorg.libXext
-	xorg.libXrender
-      	xorg.libXtst
-      	xorg.libXi
-      	nss
-      	pango
-      	zlib
-	nvidia-vaapi-driver
-        libglvnd
-      ];
-      runScript = "bash";
-    })
-
-    # Vivado
-    (pkgs.buildFHSEnvBubblewrap {
-      name = "vivado-fhs";
-      chdir = "/home/lfour/xilinx";
-      targetPkgs = pkgs: with pkgs; [
-        bash
-        coreutils
-        util-linux
-        zlib
-        ncurses5
-	libusb1
-	openssl
-	gawk
-	findutils
-	diffutils
-        libuuid
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrender
-        xorg.libXtst
-        xorg.libXi
-        xorg.libxcb
-        xorg.libXau
-        xorg.libXdmcp
-        xorg.libXcursor
-        xorg.libXfixes
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXft
-        xorg.libSM
-        xorg.libICE
-        gtk2
-        gtk3
-        glib
-        libxml2
-        libxslt
-        tcl
-        tk
-        nss
-        nspr
-        expat
-        cups
-	procps
-	libcxxStdenv
-	gccStdenv
-	glibc
-	xdg-utils
-        fontconfig
-        freetype
-        alsa-lib
-      ];
-      runScript = "bash";
-    })
-  ###### END ###################
-  ];
+    ###### END ###################
+  ] ++ (with pkgs.kdePackages; [
+    qt3d qtsvg qtdoc
+    qt6ct qtmqtt qtgrpc
+    qtbase qttools qtspell
+    qtscxml qt6gtk2 qtspeech
+    qtlottie qtgraphs qtcharts
+    qtwebview qtwayland qtsensors
+    qtquick3d qt5compat qtlocation
+    qtkeychain qtwebengine qtutilities
+    qtserialbus qtdatavis3d qtwebsockets
+    qtwebchannel qtserialport qtmultimedia
+    qthttpserver qtshadertools qtpositioning
+    qtnetworkauth qtdeclarative qttranslations
+    qtimageformats qtconnectivity qtremoteobjects
+    qtquicktimeline qtquick3dphysics qtpbfimageplugin
+    qtlanguageserver qtvirtualkeyboard qtquickeffectmaker
+    ]);
+    #
+    ###### END ###################
 }
+
