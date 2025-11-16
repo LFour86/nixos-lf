46adb8e4b970f1bacf7afea777bf0f932fc1ed15
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-16 11:32:27 +0800

    Add CUDA packages to NVIDIA configuration
    Added various CUDA packages for enhanced NVIDIA support.

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -2,9 +2,14 @@
 {
   # Enable OpenGL
   hardware.graphics.enable = true;
-  
+
+  # NVIDIA Driver version
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-    
+
+  # NVIDIA Container
+  hardware.nvidia-container-toolkit.enable = true;
+
+  # NVIDIA Prime
   hardware.nvidia.prime = {
     #offload = {
       #enable = true;
@@ -30,9 +35,66 @@
   # Fixed
   #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
     
-  #disable nouveau
+  #Disable nouveau
   boot.kernelParams = [ "modprobe.blacklist=nouveau" "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
     
   #Screen Tearing Issues
   hardware.nvidia.forceFullCompositionPipeline = true;
+
+  environment.systemPackages = with pkgs; [
+    cudaPackages.tensorrt
+    cudaPackages.nvcomp
+    cudaPackages.nsight_systems
+    cudaPackages.nsight_compute
+    cudaPackages.nccl-tests
+    cudaPackages.nccl
+    cudaPackages.libnvtiff
+    cudaPackages.libnvshmem
+    cudaPackages.libnvjpeg_2k
+    cudaPackages.libnvjpeg
+    cudaPackages.libnvjitlink
+    cudaPackages.libnvfatbin
+    cudaPackages.libnpp_plus
+    cudaPackages.libnpp
+    cudaPackages.libcutensor
+    cudaPackages.libcusparse_lt
+    cudaPackages.libcusparse
+    cudaPackages.libcusolvermp
+    cudaPackages.libcusolver
+    cudaPackages.libcurand
+    cudaPackages.libcufile
+    cudaPackages.libcufft
+    cudaPackages.libcudss
+    cudaPackages.libcublasmp
+    cudaPackages.libcublas
+    cudaPackages.imex
+    cudaPackages.gdrcopy
+    cudaPackages.fabricmanager
+    cudaPackages.cutlass
+    cudaPackages.cuquantum
+    cudaPackages.cudnn-frontend
+    cudaPackages.cudnn
+    cudaPackages.cudatoolkit
+    cudaPackages.cuda_sanitizer_api
+    cudaPackages.cuda_profiler_api
+    cudaPackages.cuda_opencl
+    cudaPackages.cuda_nvvp
+    cudaPackages.cuda_nvtx
+    cudaPackages.cuda_nvrtc
+    cudaPackages.cuda_nvprune
+    cudaPackages.cuda_nvprof
+    cudaPackages.cuda_nvml_dev
+    cudaPackages.cuda_nvdisasm
+    cudaPackages.cuda_nvcc
+    cudaPackages.cuda_nsight
+    cudaPackages.cuda_gdb
+    cudaPackages.cuda_documentation
+    cudaPackages.cuda_demo_suite
+    cudaPackages.cuda_cuxxfilt
+    cudaPackages.cuda_cupti
+    cudaPackages.cuda_cuobjdump
+    cudaPackages.cuda_cudart
+    cudaPackages.cuda_cccl
+    cudaPackages.cuda-samples
+  ];
 }
