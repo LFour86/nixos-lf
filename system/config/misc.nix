fe434e59e3b885b37668643a22e66f61f8d96b50
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-17 22:49:40 +0800

    Refactor font configuration in misc.nix
    Updated font configuration to include additional fonts and improved structure.

--- system/config/misc.nix
+++ system/config/misc.nix
@@ -1,31 +1,37 @@
 { config, pkgs, ... }:
+
 {
-  # Enable Flake
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Linux Latest Kernel
+  # Latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # For Development/Debugging
-  #environment.enableDebugInfo = true;
-
-  # Set Timezone.
   time.timeZone = "Asia/Shanghai";
 
-  # Set Fonts
-  fonts.fontDir.enable = true;
-  fonts.packages = with pkgs; [
-	maple-mono.NF
-	maple-mono.NF-CN
-	nerd-fonts.jetbrains-mono
-	minecraftia
-	font-awesome
-	nerd-fonts.ubuntu
-	nerd-fonts.ubuntu-mono
-	wqy_zenhei
-  ];
+  # Valid font config for Chinese + English + Nerd Fonts
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      maple-mono.NF
+      maple-mono.NF-CN
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.ubuntu-mono
+      nerd-fonts.ubuntu
+      wqy_zenhei
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      font-awesome
+    ];
+  };
 
-  #System Version
+  # Required fallback to avoid GNOME/KDE fcitx5 segmentation bugs
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = [ "Noto Sans CJK SC" "WenQuanYi Zen Hei" ];
+    serif     = [ "Noto Serif CJK SC" ];
+    monospace = [ "Maple Mono NF" ];
+  };
+
   system.stateVersion = "25.11";
 }
 
