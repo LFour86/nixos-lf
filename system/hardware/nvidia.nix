7f4410f7a8ef0111feb0d9f339e704a263e71789
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-22 15:10:38 +0800

    Refactor NVIDIA package settings in nvidia.nix
    Updated NVIDIA package configuration to streamline CUDA toolkit and library installations.

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -7,7 +7,7 @@
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
 
   # NVIDIA Container
-  hardware.nvidia-container-toolkit.enable = true;
+  #hardware.nvidia-container-toolkit.enable = true;
 
   # NVIDIA Prime
   hardware.nvidia.prime = {
@@ -41,60 +41,35 @@
   #Screen Tearing Issues
   hardware.nvidia.forceFullCompositionPipeline = true;
 
-  environment.systemPackages = with pkgs; [
-    cudaPackages.tensorrt
-    cudaPackages.nvcomp
-    cudaPackages.nsight_systems
-    cudaPackages.nsight_compute
-    cudaPackages.nccl-tests
-    cudaPackages.nccl
-    cudaPackages.libnvtiff
-    cudaPackages.libnvshmem
-    cudaPackages.libnvjpeg_2k
-    cudaPackages.libnvjpeg
-    cudaPackages.libnvjitlink
-    cudaPackages.libnvfatbin
-    cudaPackages.libnpp_plus
-    cudaPackages.libnpp
-    cudaPackages.libcutensor
-    cudaPackages.libcusparse_lt
-    cudaPackages.libcusparse
-    cudaPackages.libcusolvermp
-    cudaPackages.libcusolver
-    cudaPackages.libcurand
-    cudaPackages.libcufile
-    cudaPackages.libcufft
-    cudaPackages.libcudss
-    cudaPackages.libcublasmp
-    cudaPackages.libcublas
-    cudaPackages.imex
-    cudaPackages.gdrcopy
-    cudaPackages.fabricmanager
-    cudaPackages.cutlass
-    cudaPackages.cuquantum
-    cudaPackages.cudnn-frontend
-    cudaPackages.cudnn
-    cudaPackages.cudatoolkit
-    cudaPackages.cuda_sanitizer_api
-    cudaPackages.cuda_profiler_api
-    cudaPackages.cuda_opencl
-    cudaPackages.cuda_nvvp
-    cudaPackages.cuda_nvtx
-    cudaPackages.cuda_nvrtc
-    cudaPackages.cuda_nvprune
-    cudaPackages.cuda_nvprof
-    cudaPackages.cuda_nvml_dev
-    cudaPackages.cuda_nvdisasm
-    cudaPackages.cuda_nvcc
-    cudaPackages.cuda_nsight
-    cudaPackages.cuda_gdb
-    cudaPackages.cuda_documentation
-    cudaPackages.cuda_demo_suite
-    cudaPackages.cuda_cuxxfilt
-    cudaPackages.cuda_cupti
-    cudaPackages.cuda_cuobjdump
-    cudaPackages.cuda_cudart
-    cudaPackages.cuda_cccl
-    cudaPackages.cuda-samples
+  environment.systemPackages = with pkgs.cudaPackages; [
+    # ---- Core CUDA Toolkit ----
+    cudatoolkit cuda_cccl cuda_cudart
+    cuda_nvcc cuda_nvrtc cuda_nvprune
+    # ---- cuDNN / High-Perf Math Libraries ----
+    cudnn cudnn-frontend libcublas
+    libcublasmp libcudss libcusparse
+    libcusparse_lt libcusolver libcusolvermp
+    libcutensor libcurand libcufft
+    # ---- Image / Video / NPP ----
+    libnpp libnpp_plus libnvjpeg
+    libnvjpeg_2k libnvtiff libnvjitlink
+    libnvfatbin
+    # ---- Communication / Distributed ----
+    nccl nccl-tests libnvshmem
+    gdrcopy fabricmanager
+    # ---- Quantum / Numerical ----
+    cuquantum cutlass imex
+    # ---- Debug / Sanitizers / Profilers ----
+    cuda_gdb cuda_nvprof cuda_nvtx
+    cuda_profiler_api cuda_sanitizer_api cuda_opencl
+    # ---- Nsight Tools ----
+    nsight_systems nsight_compute cuda_nsight
+    cuda_nvvp
+    # ---- Diagnostics / Utils ----
+    cuda_documentation cuda_demo_suite #cuda-samples
+    cuda_cupti cuda_cuobjdump cuda_nvdisasm
+    cuda_nvml_dev cuda_cuxxfilt
+    # ---- TensorRT / Misc ----
+    tensorrt nvcomp
   ];
 }
