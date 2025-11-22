8e7fb97ea0885d4d2bb297f4b5cb836f2ed09063
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-22 15:08:01 +0800

    Add Twitter Color Emoji to font configuration

--- system/config/misc.nix
+++ system/config/misc.nix
@@ -22,6 +22,8 @@
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       font-awesome
+      #noto-fonts-color-emoji
+      twitter-color-emoji
     ];
   };
 
@@ -30,7 +32,14 @@
     sansSerif = [ "Noto Sans CJK SC" "WenQuanYi Zen Hei" ];
     serif     = [ "Noto Serif CJK SC" ];
     monospace = [ "Maple Mono NF" ];
+    emoji     = [ "Twitter Color Emoji" ];
   };
+
+  # Fcitx5
+  #fonts.fontconfig.conf = pkgs.makeFontsConf {
+    #name = "emoji-fallback";
+    #extraFonts = [ pkgs.twitter-color-emoji ];
+  #};
 
   system.stateVersion = "25.11";
 }
