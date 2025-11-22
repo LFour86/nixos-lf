f720fe6a588591a71ec8adc4a5a185394e15dd0e
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-22 15:12:32 +0800

    Update fhs.nix

--- system/programs/fhs.nix
+++ system/programs/fhs.nix
@@ -7,133 +7,123 @@
       name = "matlab-fhs";
       chdir = "/home/lfour/FHS/matlab";
       targetPkgs = pkgs: with pkgs; [
-        cacert
-        alsa-lib # libasound2
-        atk
-        glib
-        glibc
-        cairo
-        cups
-        dbus
-        fontconfig
-        gdk-pixbuf
-        gst_all_1.gst-plugins-base
-        gst_all_1.gstreamer
-        gtk3
-        nspr
-        nss
-        pam
-        pango
-        python3
-        libselinux
-        libsndfile
-        glibcLocales
-        procps
-        unzip
-        zlib
-        linux-pam
-        gtk2
-        at-spi2-atk
-        at-spi2-core
-        libdrm
+        alsa-lib atk glib
+        glibc cacert cairo
+        cups dbus fontconfig
+        gdk-pixbuf gst_all_1.gst-plugins-base gst_all_1.gstreamer
+        gtk3 nspr nss
+        pam pango python3
+        procps libselinux libsndfile
+        glibcLocales unzip zlib
+        linux-pam gtk2 at-spi2-atk
+        at-spi2-core libdrm
+
         # Required by Simulink
-        mesa
-        gcc
-        gfortran
+        mesa gcc gfortran
+
         # NixOS specific
-        udev
-        jre
-        ncurses # Needed for CLI
+        udev jre ncurses # Needed for CLI
         # Keyboard input may not work in simulink otherwise
-        libxkbcommon
-        xkeyboard_config
-        libglvnd
-        libuuid
-        libxcrypt
-        libxcrypt-legacy
+        libxkbcommon xkeyboard_config libglvnd
+        libuuid libxcrypt libxcrypt-legacy
         libgbm
         ] ++ (with pkgs.xorg; [
-          libSM
-          libX11
-          libxcb
-          libXcomposite
-          libXcursor
-          libXdamage
-          libXext
-          libXfixes
-          libXft
-          libXi
-          libXinerama
-          libXrandr
-          libXrender
-          libXt
-          libXtst
-          libXxf86vm
-          libICE
+          libSM libX11 libxcb
+          libXcomposite libXcursor libXdamage
+          libXext libXfixes libXft
+          libXi libXinerama libXrandr
+          libXrender libXt libXtst
+          libXxf86vm libICE
         ]);
       runScript = "bash";
     })
 
-    # Vivado
-    (pkgs.buildFHSEnvBubblewrap {
-      name = "vivado-fhs";
-      chdir = "/home/lfour/FHS/xilinx";
+    (buildFHSEnvBubblewrap {
+    #Note: The latest 2025.1 version has been tested and is confirmed to work.
+    #After installation is complete, 
+    #you must copy a copy of libtinfo.so.5 into ./Xilinx/2025.1/Vivado/lib/lnx64.o.
+    #The file libtinfo.so.5 can be found in the Vivado installation directory, 
+    #for example under ./Xilinx/2025.1/Vivado/lib/lnx64.o/Ubuntu/24.
+      name = "vivado-fhs"; 
       targetPkgs = pkgs: with pkgs; [
-        bash
-        coreutils
-	curl
-	doxygen
-        util-linux
-	udev
-        zlib
-        ncurses5
-        libusb1
-        openssl
-	ocl-icd
-        gawk
-        findutils
-        diffutils
-        libuuid
-	libdrm
-	valgrind
-        xorg.libX11
-        xorg.libXext
-        xorg.libXrender
-        xorg.libXtst
-        xorg.libXi
-        xorg.libxcb
-        xorg.libXau
-        xorg.libXdmcp
-        xorg.libXcursor
-        xorg.libXfixes
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXft
-        xorg.libSM
-        xorg.libICE
-        gtk2
-        gtk3
-        glib
-        libxml2
-        libxslt
-        tcl
-        tk
-        nss
-        nspr
-        expat
-        cups
-        procps
-        libcxxStdenv
-        gccStdenv
-        glibc
-        xdg-utils
-        fontconfig
+        bash bc coreutils
+        expat dbus file
+	unzip which git
+	gnumake graphviz zlib
+	tcl ncurses nettools
+	libuuid libusb1
+
+        # Toolchain
+        gcc libcxxStdenv stdenv.cc.cc
+
+        # OpenGL + X11
+        mesa libglvnd libxcrypt-legacy
+        ocl-icd opencl-headers
+
+        # glib / gtk
+        glib gtk3 fontconfig
         freetype
-        alsa-lib
+
+        # Xorg libs
+        xorg.libX11 xorg.libXext xorg.libXtst
+        xorg.libXrender xorg.libXrandr xorg.libXi
+        xorg.libXft xorg.libxcb xorg.libSM
+        xorg.libICE xorg.libXfixes xorg.libXdamage
+        xorg.libXcomposite xorg.libXcursor xorg.libxshmfence
       ];
+
+      extraBwrapArgs = [
+        "--bind" "/run/udev" "/run/udev"
+        "--bind-try" "/var/run/dbus" "/var/run/dbus"
+        "--dev-bind" "/dev" "/dev"
+      ];
+
       runScript = "bash";
+
+      profile = ''
+        export FHS=1
+        export LANG=en_US.UTF-8
+        export LC_ALL=en_US.UTF-8
+        export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true"
+	export LD_LIBRARY_PATH=/lib:/usr/lib64:/run/opengl-driver/lib:${pkgs.lib.makeLibraryPath (with pkgs; [
+	  coreutils
+          libusb1
+          ncurses
+          libxcrypt-legacy
+          glib
+          gtk3
+	  graphviz
+          fontconfig
+          freetype
+	  nettools
+	  stdenv.cc.cc
+          xorg.libX11
+          xorg.libXext
+          xorg.libXtst
+          xorg.libXrender
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libXft
+          xorg.libxcb
+        ])}:$LD_LIBRARY_PATH
+      '';
     })
-  ###### END ###################
   ];
+
+  services.udev.extraRules = ''
+    # Xilinx Platform Cable
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0008", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0007", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0009", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000d", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="000f", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0013", TAG+="uaccess"
+    ATTRS{idVendor}=="03fd", ATTRS{idProduct}=="0015", TAG+="uaccess"
+
+    # Digilent / FTDI
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="1443", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", TAG+="uaccess"
+  '';
 }
 
