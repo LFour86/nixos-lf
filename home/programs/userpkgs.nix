60ca31406c9f550fcfe97fb389e91d0c8831ab58
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-22 15:04:50 +0800

    Refactor userpkgs.nix for better organization

--- home/programs/userpkgs.nix
+++ home/programs/userpkgs.nix
@@ -2,70 +2,23 @@
 {
   home.packages = with pkgs;[
     # Daily Applications
-    bilibili-local
-    firefox
-    google-chrome
-    libreoffice
-    motrix
-    spotify
-    scrcpy
-    sillytavern
-    wpsoffice-cn
+    baidupcs-go bilibili-local codex
+    firefox gemini-cli google-chrome
+    imv libreoffice motrix
+    mpv spotify scrcpy
+    sillytavern wpsoffice-cn yazi
 
     # Chat
-    qq
-    wechat-uos
-    wemeet
+    qq wechat-uos #wemeet
 
     # Pitcure editor
-    gimp
-    krita
-    inkscape
-
-    # Ventoy
-    #ventoy-full
-    #ventoy-full-qt
+    gimp krita inkscape
 
     # OBS-Studio
     obs-studio
-    obs-studio-plugins.wlrobs
-    obs-studio-plugins.waveform
-    obs-studio-plugins.obs-tuna
-    obs-studio-plugins.obs-vaapi
-    obs-studio-plugins.obs-teleport
-    obs-studio-plugins.obs-hyperion
-    obs-studio-plugins.obs-websocket
-    obs-studio-plugins.obs-vkcapture
-    obs-studio-plugins.obs-gstreamer
-    obs-studio-plugins.obs-3d-effect
-    obs-studio-plugins.input-overlay
-    obs-studio-plugins.obs-multi-rtmp
-    obs-studio-plugins.obs-mute-filter
-    obs-studio-plugins.obs-text-pthread
-    obs-studio-plugins.obs-source-clone
-    obs-studio-plugins.obs-shaderfilter
-    obs-studio-plugins.obs-source-record
-    obs-studio-plugins.obs-replay-source
-    obs-studio-plugins.obs-livesplit-one
-    obs-studio-plugins.obs-freeze-filter
-    obs-studio-plugins.looking-glass-obs
-    obs-studio-plugins.obs-vintage-filter
-    obs-studio-plugins.obs-scale-to-sound
-    obs-studio-plugins.obs-composite-blur
-    obs-studio-plugins.obs-command-source
-    obs-studio-plugins.obs-advanced-masks
-    obs-studio-plugins.obs-source-switcher
-    obs-studio-plugins.obs-move-transition
-    obs-studio-plugins.obs-gradient-source
-    obs-studio-plugins.obs-transition-table
-    obs-studio-plugins.obs-backgroundremoval
-    obs-studio-plugins.advanced-scene-switcher
-    obs-studio-plugins.obs-pipewire-audio-capture
 
     # Network Proxy
-    clash-rs
-    clash-verge-rev
-    sing-box
+    clash-rs clash-verge-rev sing-box
 
     # Editor
     #zed-editor
@@ -80,8 +33,7 @@
     arduino-ide
 	
     # EDA
-    kicad
-    kicadAddons.kikit
+    kicad kicadAddons.kikit
 
     # CAD
     freecad
@@ -108,14 +60,28 @@
     qtcreator
 
     # Matlab
-    octaveFull
-    scilab-bin
+    octaveFull scilab-bin
+
+    # Wine
+    wineWowPackages.stable winetricks
 
     # Game
-    melonDS
-    mgba
-    ppsspp
+    melonDS mgba ppsspp
     prismlauncher
-  ];
+    ###### END ######
+  ] ++ ( with obs-studio-plugins; [
+  # Obs-Studio plugins
+    wlrobs waveform obs-tuna
+    obs-vaapi obs-teleport obs-hyperion
+    obs-websocket obs-vkcapture obs-gstreamer
+    obs-3d-effect input-overlay obs-multi-rtmp
+    obs-mute-filter obs-text-pthread obs-source-clone
+    obs-shaderfilter obs-source-record obs-replay-source
+    obs-livesplit-one obs-freeze-filter looking-glass-obs
+    obs-vintage-filter obs-scale-to-sound obs-composite-blur
+    obs-command-source obs-advanced-masks obs-source-switcher
+    obs-move-transition obs-gradient-source obs-transition-table
+    obs-backgroundremoval advanced-scene-switcher obs-pipewire-audio-capture
+  ]);
 }
 
