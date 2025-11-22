ae7f887b0c08f0b1a2c3366ff6bb898f73fc33ef
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-22 15:05:17 +0800

    add Mako service settings
    Override vscode package to include Wayland text input arguments and add Mako service settings.

--- home/programs/programs.nix
+++ home/programs/programs.nix
@@ -1,8 +1,11 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
+
 {
-  # Vscode
   programs.vscode = {
     enable = true;
+    package = pkgs.vscode.override {
+      commandLineArgs = [ "--wayland-text-input-version=3" ];
+    };
     profiles.default.extensions = with pkgs.vscode-extensions; [
       ms-vscode.makefile-tools
       ms-vscode.cpptools-extension-pack
@@ -22,6 +25,24 @@
       ms-python.vscode-pylance
       platformio.platformio-vscode-ide
     ];
+  };
+
+  # Notice
+  services.mako = {
+    enable = true;
+    settings = {
+      anchor = "top-right";
+      width =350;
+      default-timeout = 5000; # ms
+      background-color="#1e1e2e";
+      text-color="#cdd6f4";
+      border-color="#cba6f7";
+      progress-color="over #313244";
+      border-size = 2;
+      border-radius = 8;
+      padding = 12;
+      font = "Maple Mono NF 10";
+    };
   };
 }
 
