6c3a039fe8122246593f1f60415c3b7376556a6f
LFour <63118374+LFour86@users.noreply.github.com>
2025-11-29 16:12:12 +0800

    home.stateVersion: 25.11 -> 26.05

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  #Let Home Manager install and manage itself.
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   home.username = "lfour";
   home.homeDirectory = "/home/lfour";
