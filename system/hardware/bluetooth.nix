6e2fa16248712a6f18c8a7a10411f50b5cc60bb1
LFour <63118374+LFour86@users.noreply.github.com>
2026-01-28 00:32:00 +0800

    Bluetooth: enable features
    # Changes:
    1. Enable Experimental, FastConnectable and AutoEnable;
    2. Enable blueman service;
    3. Add boot init for bluetooth.

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -2,8 +2,22 @@
 
 {
   #enable bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
+    };
+  };
+  hardware.enableRedistributableFirmware = true;
+  services.blueman.enable = true;
+  boot.kernelModules = [ "btusb" "bluetooth" ];
   environment.systemPackages = with pkgs; [
     blueman bluez bluez-tools
   ];
