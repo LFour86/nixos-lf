76f9bb935717c542779062a82e0fe20e32fb14cb
LFour <63118374+LFour86@users.noreply.github.com>
2026-01-28 00:48:48 +0800

    Remove unused sha256
    Removed commented-out lines for nix-prefetch-url and sha256, it is unnecessary for local packages.

--- overlays/bilibili.nix
+++ overlays/bilibili.nix
@@ -5,9 +5,5 @@ in {
   bilibili-local = prev.bilibili.overrideAttrs (old: {
     version = version;
     src = ./local_apps/${fname};
-    # nix-prefetch-url file:///etc/nixos/overlays/local_apps/io.github.msojocs.bilibili_{$version}_amd64.deb
-    # 👇
-    sha256 = "1bz7b1yb7gs7n7mb7dx74807wycwm068ccr8d6wygswy7fi7fc3a";
   });
 }
-
