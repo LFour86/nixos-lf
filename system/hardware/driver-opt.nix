797747acbef735f1475d9a700acfef5d94d1895c
LFour <63118374+LFour86@users.noreply.github.com>
2026-07-22 10:37:12 +0800

    fix: update nixos hardware config

--- system/hardware/driver-opt.nix
+++ system/hardware/driver-opt.nix
@@ -5,10 +5,12 @@
   hardware = {
     acpilight.enable = true;
     brillo.enable = true;
-    parallels.enable = true;
+    #parallels.enable = true;
     ksm.enable = true;
-    sensor.hddtemp.enable = true;
+    #sensor.hddtemp = {
+      #enable = true;
+      #drives = [ "/dev/sda" ];
+    #};
     steam-hardware.enable = true;
   };
 }
-
