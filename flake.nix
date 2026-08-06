f1aafbcc56c63b981c685eba3226dc3d380b0b1c
LFour <63118374+LFour86@users.noreply.github.com>
2026-08-06 21:37:49 +0800

    feat(hermes): update hermes to 0.20.0(v2026.8.3)

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
-    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.7.30";
+    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.8.3";
 
   };
 
