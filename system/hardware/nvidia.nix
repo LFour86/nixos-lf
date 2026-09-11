55e05c3f22507cd658407fda83dd42c53de0b11c
LFour86 <lfourneen@qq.com>
2026-09-11 14:02:43 +0800

    fix format

--- system/hardware/nvidia.nix
+++ system/hardware/nvidia.nix
@@ -24,7 +24,7 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
  
   # Nvidia hardware settings
   hardware.nvidia = {
