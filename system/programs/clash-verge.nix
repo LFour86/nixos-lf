59f1b82d69b09445ae07bf889b2fe99c13b85442
LFour86 <lfourneen@qq.com>
2026-09-12 15:57:52 +0800

    feat(clash-verge): update package source to unstable version

--- system/programs/clash-verge.nix
+++ system/programs/clash-verge.nix
@@ -5,7 +5,7 @@
   # relies on. After rebuild, turn on "Service Mode" in the Clash Verge GUI.
   programs.clash-verge = {
     enable = true;
-    package = pkgs.clash-verge-rev;
+    package = pkgs.unstable.clash-verge-rev;
     serviceMode = true;
     group = "users";
   };
