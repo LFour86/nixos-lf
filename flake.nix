be9e23fe0ffad2ac30b9916dd67195ef63103e08
LFour86 <lfourneen@qq.com>
2026-09-22 16:47:08 +0800

    hermes-agent: update to v2026.9.21

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
-    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.9.14";
+    hermes-agent.url = "github:NousResearch/hermes-agent/v2026.9.21";
 
     llm-agents.url = "github:numtide/llm-agents.nix";
   };
